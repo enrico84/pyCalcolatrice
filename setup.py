99d6014d7c70a0b619366fa92419bc0d40af17c5
enrico84 <enrico8484@gmail.com>
2017-05-18 15:34:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ config = {
     'download_url': 'https://github.com/enrico84/pyCalcolatrice',
     'author_email': 'enrico8484@gmail.com',
     'version': '1.0',
-    'install_requires': ['nose, SLOCCount,Clone Digger,nosetests & nosexcover, pylint,coverage report, 				  pyflakes'],
+    'install_requires': ['nose'],
     'packages': find_packages(),
     'scripts': [],
     'name': 'pyCalcolatrice'
