bffc248a93019773f58ac8c46371a94b909e84c5
enrico84 <enrico8484@gmail.com>
2017-05-19 16:05:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,12 @@ def long_description():
         readme = f.read()
     return readme
 
-setup(description: 'moduli di una semplice calcolatrice in python',
+setup(name= 'source',
+      description= 'moduli di una semplice calcolatrice in python',
       long_description=long_description(),
-      author: 'Enrico Capone',
-      url: 'https://github.com/enrico84/pyCalcolatrice',
-      download_url: 'https://github.com/enrico84/pyCalcolatrice',
-      author_email: 'enrico8484@gmail.com',
-      version: '1.0',
-      packages: find_packages(),
-      scripts: [],
-      name: 'source')
+      author= 'Enrico Capone',
+      url= 'https://github.com/enrico84/pyCalcolatrice',
+      download_url= 'https://github.com/enrico84/pyCalcolatrice',
+      author_email= 'enrico8484@gmail.com',
+      version= '1.0',
+      packages= find_packages())
